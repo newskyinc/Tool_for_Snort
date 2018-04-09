da42e8249ba4b4c4907e7022e603cde14d96c5be
AaronWuDev <aaron.wu@newskysecurity.com>
2018-04-09 11:01:23 -0700

    Update ReadRuleDict.java

--- src/ReadRuleDict.java
+++ src/ReadRuleDict.java
@@ -8,9 +8,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.runners.Parameterized.Parameter;
-
 import com.mysql.jdbc.Field;
 
 public class ReadRuleDict {
@@ -18,13 +16,12 @@ public class ReadRuleDict {
 	private Map<String, String> map;
 
 	public ReadRuleDict() {
-		map = new HashMap<>();  		// constructor
+		map = new HashMap<>(); 
 	}
 	
 	protected void initReadMapLocal(String local_path) throws IOException {
 	
 		File file = new File(local_path);
-		
 		/**
 		 * jar run will use path /User/aaron/..
 		 * */	
@@ -33,49 +30,35 @@ public class ReadRuleDict {
 		if (file.exists()) {
 			file = new File(local_path);
 			System.err.println("[1] Read success -- " + current + "/snortplus.txt");
-			
 		} else {
-			
 			file = new File(current + "/snortplus.txt");   // <-----------
 			if (file.exists()) {
 				System.err.println("[2] Read success -- " + current + "/support_file/snortplus.txt");
 			} 
-			
-			else {
-				
+			else {	
 				String currentDir = System.getProperty("user.dir");
 				file = new File(currentDir + "/support_file/snortplus.txt");  // <-----------
 				if (file.exists()) {
 					System.err.println("[3] Read -- " + currentDir + "/support_file/snortplus.txt");
-				} else {
-					
-					
+				} else {	
 					System.err.println("======= USE BACKUP RULE FILE IN /support_file/backuo.txt ========");
 					file = new File(currentDir + "/support_file/backup.txt");
-					
-					
 				}
 			}
 		}
-		
-		
-		
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		
 		String line, key = "", value = "";		
 		System.err.println("\n\n--$ ReadRuleDict.java  --- initReadMapLocal() by '/latest_rule/snortplus.txt' file $---- ");
 		
-		while ((line = br.readLine()) != null) {
-			
+		while ((line = br.readLine()) != null) {	
 			if (line.contains("# Rule -")) {
 				String[] arr = line.split(" - ");
 				key = arr[1];
-				
 			} else if (line.contains("# $Family_comment")) {
 				String[] arr = line.split(" -");
 				value += arr[1];
-				
 			} else if (line.contains("# $Variant_comment")) {
 				String[] arr = line.split(" -");
 				value += arr[1];
@@ -87,7 +70,6 @@ public class ReadRuleDict {
 		System.out.println("--$------------------------------$--");
 	}
 
-	
 /*	
 	protected void initReadMapOnline(String url_path) throws IOException {	
 		URL oracle = new URL(url_path);
@@ -99,13 +81,7 @@ public class ReadRuleDict {
 		in.close();
 	}
 */
-	
-	
-	
-
-	
-	
-	
+		
 	protected String getCommentByRule(String ruleKey) {
 		if (ruleKey == null || ruleKey.isEmpty()) return null;		
 		if (!map.containsKey(ruleKey)) return "> No such rule. Comment not available yet.";
@@ -116,31 +92,11 @@ public class ReadRuleDict {
 		return map;
 	}
 	
-
-	
-	
-	
 	public static void main(String[] args) throws IOException {
-		ReadRuleDict rd = new ReadRuleDict();
-		
-		
+		ReadRuleDict rd = new ReadRuleDict();	
 		rd.initReadMapLocal("/latest_rule/snortplus.txt");
-		
-		
 		// "C:\\Users\\pankaj\\Desktop\\test.txt"
 		// rd.initReadMapOnline("https://github.com/newskyinc/Tool_for_Snort/blob/master/snortplus/snortplus.txt");
-		
-		
-		
-		
-		
 		System.out.println( rd.getCommentByRule("IOTExploit.Hikvision.ACBypass") );
 	}
-	
 }
-
-
-
-
-
-
